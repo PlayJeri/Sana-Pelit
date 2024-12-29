2b078e7365d0b3290adb3c6ffb5317a33c29a5e1
Jeri Nurmi <nurmi.jeri@gmail.com>
2024-12-29 17:20:19 +0200

    reinit front with shadcn

--- word-games-app/frontend/src/App.tsx
+++ word-games-app/frontend/src/App.tsx
@@ -1,20 +1,23 @@
 import { useState } from "react";
+import { Button } from "./components/ui/button";
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
 		<>
-			<div>
-				<h1 className="text-3xl font-bold underline text-green-500">
-					Vite + React
-				</h1>
-				<div>
-					<button onClick={() => setCount((count) => count + 1)}>
-						count is {count}
-					</button>
-				</div>
+			<h1>Vite + React</h1>
+			<div className="card">
+				<button onClick={() => setCount((count) => count + 1)}>
+					<Button>{count}</Button>
+				</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
 			</div>
+			<p className="read-the-docs">
+				Click on the Vite and React logos to learn more
+			</p>
 		</>
 	);
 }
